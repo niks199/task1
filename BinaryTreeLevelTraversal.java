9a4ca2510513cd7ca2d763a3f36fc463d05fc44f
niks199 <31590772+niks199@users.noreply.github.com>
2018-05-02 09:57:26 +1000

    Create BinaryTreeLevelTraversal.java

---
+++ BinaryTreeLevelTraversal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) {
+            q.offer(root);
+        }
+        TreeNode cur;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> subAns = new LinkedList<Integer>();
+            for (int i = 0; i < size; ++i) {        // traverse nodes in the same level
+                cur = q.poll();
+                subAns.add(cur.val);                // visit the root
+                if (cur.left != null) {
+                    q.offer(cur.left);              // push left child to queue if it is not null
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);             // push right child to queue if it is not null
+                }
+            }
+            ans.add(subAns);
+        }
+        return ans;
+    }
+}
