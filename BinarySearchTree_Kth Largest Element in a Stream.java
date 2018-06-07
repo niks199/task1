5db857c880c1a3bb6a3a78364143df653d258900
niks199 <31590772+niks199@users.noreply.github.com>
2018-06-07 14:05:17 +1000

    Update BinarySearchTree_Kth Largest Element in a Stream.java

--- BinarySearchTree_Kth Largest Element in a Stream.java
+++ BinarySearchTree_Kth Largest Element in a Stream.java
@@ -24,75 +24,126 @@ class KthLargest {
     
     public KthLargest(int k, int[] nums)
     {
+        System.out.println("KthLargest k=" + k);
+        
         m_k = k;
         
-        for (int i = 0; i < nums.length(); ++i)
+        for (int i = 0; i < nums.length; ++i)
         {
-            add(nums[i]);
+            System.out.println("nums[i]=" + nums[i]);
+            
+            insertNode(m_tree, nums[i]);
         }
     }
     
     public int add(int val)
     {
-        if (m_tree == null)
-        {
-            m_tree = new Node(val);
-            
-            m_tree.m_count = 1;
-        }
-        else
-        {
-            insertNode(m_tree, val);
-        }
+        System.out.println("add val=" + val);
+        
+        insertNode(m_tree, val);
         
         return getKLargest(m_tree, m_k);
     }
     
     public void insertNode(Node rootNode, int val)
     {
-        if (rootNode != null && val >= rootNode.m_value)
+        if (m_tree == null)
+        {
+            m_tree = new Node(val);
+            
+            m_tree.m_count = 1;
+            
+            System.out.println("insertNode root node val=" + val);
+            
+            return;
+        }
+
+        if (rootNode != null)
         {
             rootNode.m_count += 1;
             
-            if (rootNode.right != null)
+            System.out.println("insertNode rootNode.m_value=" + 
+                               rootNode.m_value + "rootNode.m_count=" + rootNode.m_count);
+        }
+        
+        if (rootNode != null && val >= rootNode.m_value)
+        {
+            if (rootNode.m_right != null)
             {
-                insertNode(rootNode.right, val);
+                System.out.println("insertNode go right val=" + val + 
+                                   " rootNode.m_right.m_value=" + rootNode.m_right.m_value +
+                                   " rootNode.m_right.m_count=" + rootNode.m_right.m_count);
+                
+                insertNode(rootNode.m_right, val);
             }
             else
             {
-                rootNode.right = new Node(val);
-                rootNode.right.m_count = 1;
+                System.out.println("insertNode val=" + val + " on the right");
+                
+                rootNode.m_right = new Node(val);
+                rootNode.m_right.m_count = 1;
             }
         }
         else if (rootNode != null && val < rootNode.m_value)
         {
-            rootNode.m_count += 1;
-            
-            if (rootNode.left != null)
+            if (rootNode.m_left != null)
             {
-                insertNode(rootNode.left, val);
+                System.out.println("insertNode go left val=" + val + 
+                                   " rootNode.m_left=" + rootNode.m_left.m_value +
+                                   " rootNode.m_left.m_count=" + rootNode.m_left.m_count);
+                
+                insertNode(rootNode.m_left, val);
             }
             else
             {
-                rootNode.left = new Node(val);
-                rootNode.left.m_count = 1;
+                System.out.println("insertNode val=" + val + " on the left");
+                
+                rootNode.m_left = new Node(val);
+                rootNode.m_left.m_count = 1;
             }
         }
     }
     
     public int getKLargest(Node rootNode, int k)
     {
-        if (rootNode == null)
+        System.out.println("getKLargest k=" + k);
+        
+        if (k == 0)
         {
-            return -1;
+            return rootNode.m_value;
         }
         
-        if (rootNode.m_count < k)
+        if (rootNode.m_right != null && rootNode.m_right.m_count > k)
         {
-            return -1;
-        }
+            System.out.println("getKLargest rootNode.m_right.m_value=" + rootNode.m_right.m_value +
+                              " rootNode.m_right.m_count=" + rootNode.m_right.m_count);
             
+            return getKLargest(rootNode.m_right, k);
+        }
         
-        return 0;
+        if (rootNode.m_right != null && rootNode.m_right.m_count <= k)
+        {
+            k = k - rootNode.m_right.m_count;
+
+            if (k == 0)
+            {
+                return getKLargest(rootNode.m_right, k);
+            }
+        }
+        
+        k = k - 1;
+        
+        if (k == 0)
+        {
+            return getKLargest(rootNode, k);
+        }
+        
+        
+        if (rootNode.m_left != null)
+        {
+            return getKLargest(rootNode.m_left, k);
+        }
+        
+        return -2;
     }
 }
