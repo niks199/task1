93643ffd93328778fbaf4734a4df208b1848285c
niks199 <31590772+niks199@users.noreply.github.com>
2018-06-19 20:39:56 +1000

    Create BinarySearchTree_Lowest Common Ancestor.java

---
+++ BinarySearchTree_Lowest Common Ancestor.java
@@ -0,0 +1,23 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+        {
+            return null;
+        }
+        
+        if (p.val == root.val || q.val == root.val)
+        {
+            return root;
+        }
+        
+        TreeNode left = lowestCommonAncestor(root.left, p, q); 
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null)
+        {
+            return root;
+        }
+        
+        return left != null ? left : right;
+    }
+}
